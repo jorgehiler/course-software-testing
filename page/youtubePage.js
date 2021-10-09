0190b83a3d31c43d6f1fff76cee5ae43f0a26f08
jorgehiler <andreshiler@gmail.com>
2021-10-08 22:06:25 -0500

    remove delay

--- page/youtubePage.js
+++ page/youtubePage.js
@@ -1,23 +1,23 @@
 module.exports = {
     commands: [{
         setInputs: function (artist) {
-            this.waitForElementVisible(this.elements.youtubeSearch.locateStrategy, this.elements.youtubeSearch.selector, 30000);
+            this.waitForElementVisible(this.elements.youtubeSearch.locateStrategy, this.elements.youtubeSearch.selector);
             this.api.setValue(this.elements.youtubeSearch.locateStrategy, this.elements.youtubeSearch.selector, artist, result => {
                 console.log(`The search was done successfully ${result.status}`);
             })
             return this;
         },
         clickSearch: function () {
-            this.waitForElementVisible(this.elements.youtubeSearchButton.locateStrategy, this.elements.youtubeSearchButton.selector, 30000);
+            this.waitForElementVisible(this.elements.youtubeSearchButton.locateStrategy, this.elements.youtubeSearchButton.selector);
             this.api.click(this.elements.youtubeSearchButton.locateStrategy, this.elements.youtubeSearchButton.selector,result => {
              console.log(`the element was clicked with status: ${result.status}`);
           });
           return this;
         },
         validateResult: function () {
-            this.waitForElementPresent(this.elements.resultTitle.locateStrategy, this.elements.resultTitle.selector, 30000);
+            this.waitForElementPresent(this.elements.resultTitle.locateStrategy, this.elements.resultTitle.selector);
             this.api.expect.element(this.elements.resultTitle).text.to.not.equal("");
-            this.waitForElementVisible(this.elements.resultDesk.locateStrategy, this.elements.resultDesk.selector, 30000);
+            this.waitForElementVisible(this.elements.resultDesk.locateStrategy, this.elements.resultDesk.selector);
             this.api.expect.element(this.elements.resultDesk).text.to.not.equal("");
 
             return this;
